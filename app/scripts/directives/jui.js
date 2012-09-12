41e2f3fce2565eae90fccdcdb80657d39de5b8df
William Lepinski <willsp@gmail.com>
2012-09-12 16:26:58 -0300

    Add the directive for $().droppable()

--- app/scripts/directives/jui.js
+++ app/scripts/directives/jui.js
@@ -1,114 +1,228 @@
 /*global angular:true $:true*/
 'use strict';
 
-angular.module('jui', [])
-  .directive('juiDraggable', function() {
+var jui_module = angular.module('jui', []);
+/**
+ * Define a new directive for jQueryUI Draggable.
+ */
+jui_module.directive('juiDraggable', function juiDraggable() {
     return {
-      restrict: 'A',
-      scope: {
-        onCreate: '=',
-        onStart: '=',
-        onStop: '=',
-        onDrag: '=',
-        axis: '=',
-        cancel: '=',
-        cursor: '=',
-        cursorAt: '=',
-        delay: '=',
-        distance: '=',
-        grid: '=',
-        handle: '=',
-        snap: '=',
-        iframeFix: '=',
-        refreshPositions: '=',
-        snapMode: '=',
-        snapTolerance: '=',
-        stack: '=',
-        scope: '=',
-        containment: '=',
-        scroll: '=',
-        scrollSensitivity: '=',
-        scrollSpeed: '=',
-        revert: '=',
-        revertDuration: '=',
-        helper: '=',
-        opacity: '=',
-        zIndex: '='
-      },
-      link: function postLink(scope, element, attrs) {
-        var options = {
-          axis: scope.axis,
-          cancel: scope.cancel,
-          cursor: scope.cursor,
-          cursorAt: scope.cursorAt,
-          distance: scope.distance,
-          delay: scope.delay,
-          grid: scope.grid,
-          snap: scope.snap,
-          handle: scope.handle,
-          iframeFix: scope.iframeFix,
-          refreshPositions: scope.refreshPositions,
-          scope: scope.scope,
-          snapMode: scope.snapMode,
-          snapTolerance: scope.snapTolerance,
-          stack: scope.stack,
-          containment: scope.containment,
-          scroll: scope.scroll,
-          scrollSpeed: scope.scrollSpeed,
-          scrollSensitivity: scope.scrollSensitivity,
-          revert: scope.revert,
-          revertDuration: scope.revertDuration,
-          helper: scope.helper,
-          opacity: scope.opacity,
-          zIndex: scope.zIndex
-        };
+        restrict:'A',
+        scope:{
+            onCreate:'=',
+            onStart:'=',
+            onStop:'=',
+            onDrag:'=',
+            axis:'=',
+            cancel:'=',
+            cursor:'=',
+            cursorAt:'=',
+            delay:'=',
+            distance:'=',
+            grid:'=',
+            handle:'=',
+            snap:'=',
+            iframeFix:'=',
+            refreshPositions:'=',
+            snapMode:'=',
+            snapTolerance:'=',
+            stack:'=',
+            scope:'=',
+            containment:'=',
+            scroll:'=',
+            scrollSensitivity:'=',
+            scrollSpeed:'=',
+            revert:'=',
+            revertDuration:'=',
+            helper:'=',
+            opacity:'=',
+            zIndex:'='
+        },
+        link:function postLink(scope, element, attrs) {
+            var options = {
+                axis:scope.axis,
+                cancel:scope.cancel,
+                cursor:scope.cursor,
+                cursorAt:scope.cursorAt,
+                distance:scope.distance,
+                delay:scope.delay,
+                grid:scope.grid,
+                snap:scope.snap,
+                handle:scope.handle,
+                iframeFix:scope.iframeFix,
+                refreshPositions:scope.refreshPositions,
+                scope:scope.scope,
+                snapMode:scope.snapMode,
+                snapTolerance:scope.snapTolerance,
+                stack:scope.stack,
+                containment:scope.containment,
+                scroll:scope.scroll,
+                scrollSpeed:scope.scrollSpeed,
+                scrollSensitivity:scope.scrollSensitivity,
+                revert:scope.revert,
+                revertDuration:scope.revertDuration,
+                helper:scope.helper,
+                opacity:scope.opacity,
+                zIndex:scope.zIndex
+            };
 
-        // Assigning events
-        if(scope.onCreate !== undefined){
-          options.create = function(event, ui) {
-            scope.onCreate.apply(null, arguments);
-          };
+            // Assigning events
+            if (scope.onCreate !== undefined) {
+                options.create = function (event, ui) {
+                    scope.onCreate.apply(null, arguments);
+                };
+            }
+
+            if (scope.onStart !== undefined) {
+                options.start = function (event, ui) {
+                    scope.onStart.apply(null, arguments);
+                };
+            }
+
+            if (scope.onStop !== undefined) {
+                options.stop = function (event, ui) {
+                    scope.onStop.apply(null, arguments);
+                };
+            }
+
+            if (scope.onDrag !== undefined) {
+                options.stop = function (event, ui) {
+                    scope.onDrag.apply(null, arguments);
+                };
+            }
+
+            $(element).draggable(options);
         }
-
-        if(scope.onStart !== undefined){
-          options.start = function(event, ui) {
-            scope.onStart.apply(null, arguments);
-          };
-        }
-
-        if(scope.onStop !== undefined){
-          options.stop = function(event, ui) {
-            scope.onStop.apply(null, arguments);
-          };
-        }
-
-        if(scope.onDrag !== undefined){
-          options.stop = function(event, ui) {
-            scope.onDrag.apply(null, arguments);
-          };
-        }
-
-        $(element).draggable(options);
-      }
     };
-  })
-  .directive('juiProgressbar', function(){
+});
+
+/**
+ *
+ */
+jui_module.directive('juiButton', function juiButton() {
     return {
-      restrict: 'A',
-      scope: {
-        value: '='
-      },
-      link: function postLink(scope, element, attrs) {
-        var options = {
-          value: scope.value
-        };
+        restrict:'A',
+        scope:{
+            text:'=',
+            label:'=',
+            disabled:'='
+        },
+        link:function postLink(scope, element, attrs) {
+            var options = {
+                text:scope.text,
+                disabled:scope.disabled,
+                label:scope.label
+            };
 
-        // Register a $watch on value to update the widget
-        scope.$watch('value', function(){
-          $(element).progressbar("value", parseInt(scope.value, 10));
-        });
+            // Register a $watch on value to update the widget
+            scope.$watch('text', function () {
+                $(element).button("text", parseInt(scope.value, 10));
+            });
 
-        $(element).progressbar(options);
-      }
+            scope.$watch('disabled', function () {
+                $(element).button("disabled", scope.disabled);
+            });
+
+            $(element).button(options);
+        }
     };
-  });
+});
+
+/**
+ *
+ */
+jui_module.directive('juiButtonset', function juiButtonset() {
+    return {
+        restrict:'A',
+        scope:{
+            disabled:'='
+        },
+        link:function postLink(scope, element, attrs) {
+            var options = {
+                disabled:scope.disabled
+            };
+
+            $(element).buttonset(options);
+        }
+    };
+});
+
+/**
+ *
+ */
+jui_module.directive('juiProgressbar', function juiProgressbar() {
+    return {
+        restrict:'A',
+        scope:{
+            value:'='
+        },
+        link:function postLink(scope, element, attrs) {
+            var options = {
+                value:scope.value
+            };
+
+            // Register a $watch on value to update the widget
+            scope.$watch('value', function () {
+                $(element).progressbar("value", parseInt(scope.value, 10));
+            });
+
+            $(element).progressbar(options);
+        }
+    };
+});
+
+/**
+ * Define a new directive for jQueryUI Dropabble.
+ *
+ * @link http://jqueryui.com/demos/droppable/
+ */
+jui_module.directive('juiDroppable', [function juiDroppable() {
+    var eventNames = ['onCreate', 'onActivate', 'onDeactivate', 'onOver', 'onOut'],
+        optionNames = ['disabled', 'accept', 'activeClass', 'addClasses', 'greedy', 'hoverClass', 'scope', 'tolerance'];
+
+    return {
+        restrict: "A",
+        scope: {
+            onCreate: "=eventOncreate",
+            onActivate: '=eventOnactivate',
+            onDeactivate: '=eventOndeactivate',
+            onOver: '=eventOnover',
+            onOut: '=eventOnout',
+            onDrop: '=eventOndrop',
+            disabled: '=optDisabled',
+            accept: '=optAccept',
+            activeClass: '=optActiveclass',
+            addClasses: '=optAddclasses',
+            greedy: '=optGreedy',
+            hoverClass: '=optHoverclass',
+            scope: '=optScope',
+            tolerance: '=optTolerance'
+        },
+
+        link: function(scope, element, attrs){
+            var options = {};
+
+            $(optionNames).each(function(i, optionName){
+                options[optionName] = scope[optionName];
+                scope.$watch(optionName, function(){
+                    $(element).droppable('option', optionName, scope[optionName]);
+                });
+            });
+
+            // Assign events to the options hash.
+            $(eventNames).each(function(i, scopeEvent){
+                if(scope[scopeEvent] !== undefined){
+                    var eventName = scopeEvent.replace('on', '').toLowerCase();
+                    options[eventName] = function(event, ui){
+                        if( angular.isFunction(scope[scopeEvent]) ){
+                            scope[scopeEvent].apply(null, arguments);
+                        }
+                    }
+                }
+            });
+
+            // Apply the plugin
+            $(element).droppable(options);
+        }
+    };
+}]);
--- test/spec/directives/jui.js
+++ test/spec/directives/jui.js
@@ -1,41 +1,102 @@
 'use strict';
 
-function xinspect(o,i){
-    if(typeof i=='undefined')i='';
-    if(i.length>50)return '[MAX ITERATIONS]';
-    var r=[];
-    for(var p in o){
-        var t=typeof o[p];
-        r.push(i+'"'+p+'" ('+t+') => '+(t=='object' ? 'object:'+xinspect(o[p],i+'  ') : o[p]+''));
-    }
-    return r.join(i+'\n');
-}
+describe('Module: jui', function () {
+    beforeEach(module('jui'));
+    beforeEach(function(){
+        this.addMatchers({
+            toBeEqual: function(expected) {
+                return angular.equals(this.actual, expected);
+            }
+        });
+    });
 
-describe('Module: jui', function() {
-  beforeEach(module('jui'));
+    describe('Module: jui:draggable', function () {
+        var element;
 
-  describe('Module: jui:draggable', function() {
-    var element;
+        it('should apply the zero-configuration usabe of draggable', inject(function ($rootScope, $compile) {
+            element = angular.element('<div jui:draggable></div>');
+            element = $compile(element)($rootScope);
+            expect($(element).is('.ui-draggable')).toBeTruthy();
+        }));
 
-    it('should apply the zero-configuration usabe of draggable', inject(function($rootScope, $compile) {
-      element = angular.element('<div jui:draggable></div>');
-      element = $compile(element)($rootScope);
-      expect($(element).is('.ui-draggable')).toBeTruthy();
-    }));
+        it('should trigger dragcreate event', inject(function ($rootScope, $compile) {
+            element = angular.element('<div jui:draggable on:create="onCreate"></div>');
 
-    it('should trigger dragcreate event', inject(function($rootScope, $compile) {
-      element = angular.element('<div jui:draggable on:create="onCreate"></div>');
+            $rootScope.hello = 'Hello';
+            $rootScope.onCreate = function () {
+            }
 
-      $rootScope.hello = 'Hello';
-      $rootScope.onCreate = function(){}
+            spyOn($rootScope, 'onCreate').andCallThrough();
+            element = $compile(element)($rootScope);
 
-      spyOn($rootScope, 'onCreate').andCallThrough();
-      element = $compile(element)($rootScope);
+            expect($rootScope.onCreate).toHaveBeenCalled();
+            expect($rootScope.onCreate.calls[0].args[0].type).toBe('dragcreate');
+        }));
 
-      expect($rootScope.onCreate).toHaveBeenCalled();
-      expect($rootScope.onCreate.calls[0].args[0].type).toBe('dragcreate');
-    }));
+    });
 
-  });
+    describe('Module: jui:droppable', function () {
+        var element;
+
+        it('should apply the zero-configuration usage of draggable plugin', inject(function ($rootScope, $compile) {
+            element = angular.element('<div jui:droppable></div>');
+            element = $compile(element)($rootScope);
+            expect($(element).is('.ui-droppable')).toBeTruthy();
+        }));
+
+        it('should override all options if them was defined on the DOM element', inject(function ($rootScope, $compile) {
+            element = angular.element('<div jui:droppable ' +
+                'opt:disabled="true" ' +
+                'opt:activeClass="\'ui-state-highlight\'" ' +
+                'opt:accept="\'.test\'" ' +
+                'opt:addClasses="false" ' +
+                'opt:greedy="true" ' +
+                'opt:hoverClass="true" ' +
+                'opt:tolerance="\'pointer\'" ' +
+                'opt:scope="\'tasks\'"></div>');
+            element = $compile(element)($rootScope);
+
+            expect($(element).droppable('option')).toBeEqual({
+                disabled : true,
+                accept : '.test',
+                activeClass : 'ui-state-highlight',
+                addClasses : false,
+                greedy : true,
+                hoverClass : true,
+                scope : 'tasks',
+                tolerance : 'pointer'
+            });
+        }));
+
+        it('should modify options of the plugin when something is changed on the scope (raw)', inject(function($rootScope, $compile){
+            $rootScope.activeClass = 'ui-state-highlight';
+
+            element = angular.element('<div jui:droppable opt:activeClass="activeClass"></div>');
+            element = $compile(element)($rootScope);
+
+            expect($(element).droppable('option', 'activeClass')).toBe('ui-state-highlight');
+
+            $rootScope.activeClass = 'ui-state-highlight2';
+            $rootScope.$digest();
+            expect($(element).droppable('option', 'activeClass')).toBe('ui-state-highlight2');
+        }));
+
+        it('should modify options of the plugin when something is changed on the scope (function)', inject(function($rootScope, $compile){
+            $rootScope.activeClass = function(){
+                return 'ui-state-highlight';
+            };
+
+            element = angular.element('<div jui:droppable opt:activeClass="activeClass()"></div>');
+            element = $compile(element)($rootScope);
+
+            expect($(element).droppable('option', 'activeClass')).toBe('ui-state-highlight');
+
+            $rootScope.activeClass = function(){
+                return 'ui-state-highlight2';
+            };
+            $rootScope.$digest();
+            expect($(element).droppable('option', 'activeClass')).toBe('ui-state-highlight2');
+        }));
+    });
 
 });
